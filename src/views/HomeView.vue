2e23750945d9f8a616a2abb4a231b628542ff979
Vitor <vitorpereira@profkenny.com.br>
2025-04-16 16:50:59 -0300

    feat: adiciona projeto Task Manager Web ao portfólio

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -47,11 +47,11 @@ export default {
       },
       {
         id: 3,
-        title: 'Calculadora de gorjetas',
-        description: 'Desenvolvido em html, css e javascript, o projeto tem a finalidade de mostrar as gorjetas de uma conta especifca de um cliente.',
-        image: 'https://camo.githubusercontent.com/37c9949956b9d557437e4837f6e97b81b32d3355106b55eadee158158a66dc5d/68747470733a2f2f75706c6f61646465696d6167656e732e636f6d2e62722f696d616765732f3030332f3339322f3835352f6f726967696e616c2f676f726a6574612e504e473f31363239353832383736',
-        demo: 'https://vitormariano-hub.github.io/APP-Gorjetas/',
-        github: 'https://github.com/VitorMariano-hub/APP-Gorjetas'
+        title: 'Task Manager Web',
+        description: 'Aplicação de gerenciamento de tarefas em estilo Kanban, com frontend em Vue.js e backend Laravel. Permite criar, mover e excluir tarefas entre colunas com drag & drop. \n\n🔐 Acesso de teste:\n• Usuário: teste@teste.com\n• Senha: teste@123',
+        image: 'https://private-user-images.githubusercontent.com/69535241/434501404-f650a013-8f2d-46c3-8d21-e08b4511f3a1.png?jwt=eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJnaXRodWIuY29tIiwiYXVkIjoicmF3LmdpdGh1YnVzZXJjb250ZW50LmNvbSIsImtleSI6ImtleTUiLCJleHAiOjE3NDQ4MzMyMTMsIm5iZiI6MTc0NDgzMjkxMywicGF0aCI6Ii82OTUzNTI0MS80MzQ1MDE0MDQtZjY1MGEwMTMtOGYyZC00NmMzLThkMjEtZTA4YjQ1MTFmM2ExLnBuZz9YLUFtei1BbGdvcml0aG09QVdTNC1ITUFDLVNIQTI1NiZYLUFtei1DcmVkZW50aWFsPUFLSUFWQ09EWUxTQTUzUFFLNFpBJTJGMjAyNTA0MTYlMkZ1cy1lYXN0LTElMkZzMyUyRmF3czRfcmVxdWVzdCZYLUFtei1EYXRlPTIwMjUwNDE2VDE5NDgzM1omWC1BbXotRXhwaXJlcz0zMDAmWC1BbXotU2lnbmF0dXJlPWFlNmUxOTEwNjk5ZTU0OWVhMzIxMDI5YTY2ZTQ4NDA3OWUzNTg2NGY4ZjQ4MjM5YjEzZjE0ODY4N2QwZmQxZGMmWC1BbXotU2lnbmVkSGVhZGVycz1ob3N0In0.5VmqJ4CjBAi9TlzSZGzuowRcOipL-zPVjR8Jl5v5QRU',
+        demo: 'https://task-manager-web.onrender.com',
+        github: 'https://github.com/VitorMariano-hub/task-manager-app'
       }
     ]
     return {
