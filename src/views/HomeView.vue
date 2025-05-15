7839c9b1957ed6ee586d46f5ebc83c9a16b7e627
Vitor <vitorpereira@profkenny.com.br>
2025-05-15 00:58:40 -0300

    Ajuste titulo projeto solid

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -60,7 +60,7 @@ export default {
       },
       {
         id: 4,
-        title: 'Princípios SOLID da Programação Orientada a Objetos',
+        title: 'Princípios SOLID da Programação Orientada a Objetos.',
         description: 'Projeto desenvolvido com exemplos práticos dos 5 princípios do SOLID aplicados em PHP, focando em boas práticas de design, código limpo e testes automatizados.',
         image: 'https://images.ctfassets.net/bp521nfj3cq3/4pw3f1nT851svWyID3Bl46/58e6053ac0258636eef45f899c542706/Solid.png',
         github: 'https://github.com/VitorMariano-hub/solid-principles-php'
